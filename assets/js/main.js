12186e1f82bf373ad3b81e2617bace5b05f7669b
Seryyyosh <85225071+Sergey-Astafurov@users.noreply.github.com>
2025-05-21 20:14:20 +0500

    updete home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,19 @@
-
-
-
-
-const newsItem = document.querySelectorAll('.news__item');
-console.log(newsItem);
-newsItem.forEach(el => {
-  el.addEventListener('hover', (e) => {
-    console.log(e.target);
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-mobile');
+burger.addEventListener('click', () => {
+  console.log('c');
+  if (!nav.classList.contains('active')) {
+    nav.classList.add('active');
+    burger.style.overflow = 'visible';
+    document.body.style.overflow = 'hidden';
+  } else {
+    nav.classList.remove('active');
+    burger.style.overflow = 'hidden';
+    document.body.style.overflow = 'visible';
   }
-);
 })
+
+
 
 
 
@@ -48,6 +52,7 @@ const swiper = new Swiper(".mySwiper", {
 
   },
 });
+
 const swiper2 = new Swiper(".mySwiper2", {
   pagination: {
     el: ".swiper-pagination",
@@ -64,32 +69,59 @@ const swiper2 = new Swiper(".mySwiper2", {
   },
 
 });
-let swiperFacts = null;
 
+let swiperFacts;
 function initFactsSlider() {
-  const mobile = window.innerWidth <= 800;
+  const mobile = window.innerWidth <= 1200;
 
   if (mobile && !swiperFacts) {
+    console.log('object');
     swiperFacts = new Swiper(".swiper-facts", {
       slidesPerView: 'auto',
-      spaceBetween: 30,
-      pagination: {
-        el: '.sw-facts-pagination',
+      // spaceBetween: 30,
+      navigation: {
+        nextEl: ".sw-btn-next-facts",
+        prevEl: ".sw-btn-prev-facts",
       },
+
     });
-  } else if (!mobile && swiperFacts) {
+  } else if (window.innerWidth > 1200 && swiperFacts) {
     swiperFacts.destroy(true, true);
     swiperFacts = null;
   }
 }
 
-let resizeTimeout;
-window.addEventListener("resize", () => {
-  clearTimeout(resizeTimeout);
-  resizeTimeout = setTimeout(initFactsSlider, 200);
-});
+window.addEventListener('resize', initFactsSlider);
+window.addEventListener('load', initFactsSlider);
 
 initFactsSlider();
+
+let swiperNewsHome;
+function initNewsHomeSlider() {
+  const mobile = window.innerWidth <= 852;
+  if (mobile && !swiperNewsHome) {
+    console.log('news');
+    swiperNewsHome = new Swiper(".nw-swiper",
+      {
+        slidesPerView: '1',
+        breakpoints: {
+          852: {
+            slidesPerView: '4',
+          }
+        },
+      });
+  } else if (window.innerWidth > 852 && swiperNewsHome) {
+    swiperNewsHome.destroy(true, true);
+    swiperNewsHome = null;
+    console.log('off');
+  }
+}
+
+
+window.addEventListener('resize', initNewsHomeSlider);
+window.addEventListener('load', initNewsHomeSlider);
+
+initNewsHomeSlider();
 
 const swiperUsely = new Swiper(".sw-usely", {
 
@@ -112,17 +144,6 @@ const swiperUsely = new Swiper(".sw-usely", {
 
 });
 
-
-// const swiperFeature = new Swiper(".sw-feature", {
-//   slidesPerView: 1,
-//   freeMode: true,
-//   loop: true,
-
-
-//   // spaceBetween: 50,
-// });
-
-
 let swiperFeatures = null;
 
 function initFeatureSlider() {
@@ -142,14 +163,28 @@ function initFeatureSlider() {
   }
 }
 
-window.addEventListener("resize", () => {
-  clearTimeout(resizeTimeout);
-  resizeTimeout = setTimeout(initFeatureSlider, 200);
-});
+window.addEventListener('resize', initFeatureSlider);
+window.addEventListener('load', initFeatureSlider);
 
 initFeatureSlider();
 
 
+
+const swiperNews = new Swiper(".swiper-news", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".sw-news-pagination",
+  },
+  breakpoints: {
+    1124: {
+      slidesPerView: 2,
+    }
+  },
+  navigation: {
+    nextEl: ".sw-news-button-next",
+    prevEl: ".sw-news-button-prev",
+  },
+});
 
 
 
